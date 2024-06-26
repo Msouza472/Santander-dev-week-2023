38118c445aebb513efbfec9a4eed0d20967aa1fb
Msouza472 <sdsouza472souza@gmail.com>
2024-06-25 21:00:05 -0300

    Initial commit

---
+++ src/main/java/me/dio/Santander_dev_week_2023/SantanderDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_dev_week_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Santander_dev_week_2023/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_dev_week_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
