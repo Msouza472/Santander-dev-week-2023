c49a282c6b634bd8c68fbec8aa09ac948f922ecd
Msouza472 <sdsouza472souza@gmail.com>
2024-06-25 22:31:40 -0300

    Java RESTful criada para o Sanatander dev week

--- src/main/java/me/dio/Santander_dev_week_2023/Application.java
+++ src/main/java/me/dio/Santander_dev_week_2023/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDevWeek2023Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeek2023Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
